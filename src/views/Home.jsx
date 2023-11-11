b70fb8d8d04b23ecf6bb0a20d3814202d32a9a08
Yamil Castillo <yamilcs@gmail.com>
2023-11-11 01:36:09 -0300

    mensaje vista favoritos desafio-context

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,18 +1,21 @@
 import { useContext } from "react";
-import { DataContext } from '../context/DataContext';
+import { DataContext } from "../context/DataContext";
 
 import Gallery from "../components/Gallery";
 
-import Spinner from 'react-bootstrap/Spinner';
+import Spinner from "react-bootstrap/Spinner";
 
 const Home = () => {
-
-  const { isLoading } = useContext(DataContext)
+  const { isLoading } = useContext(DataContext);
 
   return (
     <div className="App">
       <h1>Natural Pic</h1>
-      { isLoading ? ( <Spinner animation="border" variant="primary" /> ) : ( <Gallery /> )  }
+      {isLoading ? (
+        <Spinner animation="border" variant="primary" />
+      ) : (
+        <Gallery />
+      )}
     </div>
   );
 };
